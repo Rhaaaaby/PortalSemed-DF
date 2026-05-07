01354d708ba189fb11d151a139e2ab7c08b2338c
2024Merivan <merivan.ribeirosantos@gmail.com>
2026-05-06 23:41:51 -0300

    Finalizando Home e estruturando navegação

---
+++ js/model.js
@@ -0,0 +1,10 @@
+const NoticiaModel = {
+    getUltimasNoticias: function() {
+        // Simulando resposta de uma API REST
+        return [
+            { titulo: "Volta às aulas 2026", resumo: "A Secretaria Municipal de Educação divulga o calendário para o início do ano letivo." },
+            { titulo: "Programa de Merenda Escolar", resumo: "Novos investimentos para a melhoria da alimentação dos alunos da rede municipal." },
+            { titulo: "Capacitação Docente", resumo: "Curso de capacitação oferecido para os profissionais da educação em São Miguel." }
+        ];
+    }
+};
